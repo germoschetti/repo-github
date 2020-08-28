3e50bf0b54e2106f4d65d1deebd3bd2fe42af81d
germoschetti <germoschetti@hotmail.com>
2020-08-28 20:26:42 -0300

    log out

--- js/login.js
+++ js/login.js
@@ -82,7 +82,7 @@
          // The ID token you need to pass to your backend:
          var id_token = googleUser.getAuthResponse().id_token;
          console.log("ID Token: " + id_token);
-         location.href = "index.html"
+         
          return true
          
        } 
