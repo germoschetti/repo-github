53620beb6ace859390a9d4113c5da60ffb627622
germoschetti <germoschetti@hotmail.com>
2020-08-29 11:30:48 -0300

    2

--- js/login.js
+++ js/login.js
@@ -65,34 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 //INICIO DE SESION CON GOOGLE
-function onSignIn(googleUser) {
-   // Useful data for your client-side scripts:
-   var profile = googleUser.getBasicProfile();
-   sessionStorage.setItem("google", googleUser)
-   sessionStorage.setItem("Usergoogle", profile.getName())
-   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-   console.log('Full Name: ' + profile.getName());
-   console.log('Given Name: ' + profile.getGivenName());
-   console.log('Family Name: ' + profile.getFamilyName());
-   console.log("Image URL: " + profile.getImageUrl());
-   console.log("Email: " + profile.getEmail());
 
-   // The ID token you need to pass to your backend:
-   var id_token = googleUser.getAuthResponse().id_token;
-   console.log("ID Token: " + id_token);
-   location.href = "index.html"
-
-   return true
-}
-// SIGN OUT GOOGLE
-function signOut() {
-      sessionStorage.removeItem("google");
-      sessionStorage.clear();
- 
-}
-
-var salir = document.getElementById("salir");
-salir.addEventListener('click', signOut)
 
 
 
