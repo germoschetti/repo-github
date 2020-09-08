a1761151688851b93e408cbd4e40dec6c598f2c2
germoschetti <germoschetti@hotmail.com>
2020-09-08 19:50:25 -0300

    1

--- js/login.js
+++ js/login.js
@@ -68,7 +68,7 @@ function onSignIn(googleUser) {
    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
    var id_token = googleUser . getAuthResponse (). id_token ;
    console.log(id_token)
-      window.location.href = "index.html"
+      
  }
 
  function signOut() {
