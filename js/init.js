a7544bcc5418a55a6b5626f76372bd13f3855892
germoschetti <germoschetti@hotmail.com>
2020-09-09 15:02:50 -0300

    probando

--- js/init.js
+++ js/init.js
@@ -46,6 +46,43 @@ var getJSONData = function (url) {
 document.addEventListener("DOMContentLoaded", function (e) {
 });
 
+
+function onSignIn(googleUser) {
+    
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+      
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    
+    sessionStorage.setItem('google', profile.getGivenName())
+    location.href = "index.html" 
+  
+      }
+
+
+function signOut() {
+var auth2 = gapi.auth2.getAuthInstance();
+auth2.signOut().then(function () {
+ window.location.href='login.html'
+ console.log('User signed out.');
+ sessionStorage.clear()
+});
+}
+
+function onload(){
+gapi.load('auth2',function(){
+  gapi.auth2.init()
+})
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
   if (sessionStorage.getItem("Usuario")|| sessionStorage.getItem("google")) {
