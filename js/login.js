2dc69af828133c0eef5272410e5773cda3e21bbc
germoschetti <germoschetti@hotmail.com>
2020-09-09 11:23:32 -0300

    2

--- js/login.js
+++ js/login.js
@@ -59,7 +59,31 @@ document.addEventListener('DOMContentLoaded', () => {
    } 
 });
 
+function onSignIn(googleUser) {
+      
+   // Useful data for your client-side scripts:
+   var profile = googleUser.getBasicProfile();
+   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+   console.log('Given Name: ' + profile.getGivenName());
+   console.log('Family Name: ' + profile.getFamilyName());
+   console.log("Image URL: " + profile.getImageUrl());
+   console.log("Email: " + profile.getEmail());
+   sessionStorage.setItem('google', profile.getGivenName())
 
+   // The ID token you need to pass to your backend:
+   var id_token = googleUser.getAuthResponse().id_token;
+   console.log("ID Token: " + id_token);
+     }
+   
+   
+
+function signOut() {
+ var auth2 = gapi.auth2.getAuthInstance();
+ auth2.signOut().then(function () {
+   console.log('User signed out.');
+   sessionStorage.clear()
+ });
+}
 
 /* function signOut() {
    var auth2 = gapi.auth2.getAuthInstance();
