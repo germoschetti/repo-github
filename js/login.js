d3a20ac30d234f91a0bf6236bc14ef78830e277e
germoschetti <germoschetti@hotmail.com>
2020-09-09 11:00:13 -0300

    2

--- js/login.js
+++ js/login.js
@@ -60,17 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function onSignIn(googleUser) {
-   var profile = googleUser.getBasicProfile();
-   sessionStorage.setItem("google", getName())
-   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-   console.log('Name: ' + profile.getName());
-   console.log('Image URL: ' + profile.getImageUrl());
-   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-   var id_token = googleUser . getAuthResponse (). id_token ;
-   console.log(id_token)
-   window.location.href = "index.html";
- }
 
 /* function signOut() {
    var auth2 = gapi.auth2.getAuthInstance();
