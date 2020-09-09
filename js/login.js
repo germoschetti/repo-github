b9c4495cdfbfbe6fa96d9c6abb9e206294b49306
germoschetti <germoschetti@hotmail.com>
2020-09-09 11:55:29 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -80,13 +80,6 @@ function onSignIn(googleUser) {
       location.href = "index.html" 
    })
 
-function signOut() {
- var auth2 = gapi.auth2.getAuthInstance();
- auth2.signOut().then(function () {
-   console.log('User signed out.');
-   sessionStorage.clear()
- });
-}
 
 /* function signOut() {
    var auth2 = gapi.auth2.getAuthInstance();
