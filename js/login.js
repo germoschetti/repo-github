681859fef299fe5705eced7473ec03978d1eba01
germoschetti <germoschetti@hotmail.com>
2020-09-09 14:50:19 -0300

    logout

--- js/login.js
+++ js/login.js
@@ -59,7 +59,20 @@ document.addEventListener('DOMContentLoaded', () => {
    } 
 });
 
+function signOut() {
+   var auth2 = gapi.auth2.getAuthInstance();
+   auth2.signOut().then(function () {
+     window.location.href='login.html'
+     console.log('User signed out.');
+     sessionStorage.clear()
+   });
+  }
 
+  function onload(){
+    gapi.load('auth2',function(){
+      gapi.auth2.init();
+    })
+  }
 /* function signOut() {
    var auth2 = gapi.auth2.getAuthInstance();
    auth2.signOut().then(function () {
