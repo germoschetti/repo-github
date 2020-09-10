fe0c8f289a1a0b7494557c833c3f2b50cc4e2e21
germoschetti <germoschetti@hotmail.com>
2020-09-10 15:47:17 -0300

    logout

--- js/login.js
+++ js/login.js
@@ -85,48 +85,9 @@ function onSignIn(googleUser) {
  }
  
  
- function signOut() {
-   sessionStorage.clear()
-   var auth2 = gapi.auth2.getAuthInstance();
-   auth2.signOut().then(function () {
-     window.location.href = 'login.html'
-     console.log('User signed out.');
-    
-   });
- }
- 
- function onload() {
-   gapi.load('auth2', function () {
-     gapi.auth2.init()
-   })
- }
 
-/*function signOut() {
-   var auth2 = gapi.auth2.getAuthInstance();
-   auth2.signOut().then(function () {
-     window.location.href='login.html'
-     console.log('User signed out.');
-     sessionStorage.clear()
-   });
-  }
 
-  function onload(){
-    gapi.load('auth2',function(){
-      gapi.auth2.init();
-    })
-  }*/
-/* function signOut() {
-   var auth2 = gapi.auth2.getAuthInstance();
-   auth2.signOut().then(function () {
-     console.log('User signed out.');
-     sessionStorage.clear();
-   });
- }
 
-var salir = document.getElementById("salir")
-salir.addEventListener('click', function(e){
-   sessionStorage.clear();
-})*/
 
 
 
